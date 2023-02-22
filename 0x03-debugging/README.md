38092b5baeb4223c103e4122ebd0659a1e9b24fc
Alvin Kiiru <alvinkiiru@gmail.com>
2023-02-22 15:23:25 +0300

    uploads for 0x03 debugging

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is a readme for the debugging part of the project, he attac, he protecc, he prevent overflow on slacc
---
+++ 0x03-debugging/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - used to test whether an integer is positive or negatve and prints
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i=98;
+	positive_or_negative(i);
+
+	return(0);
+}
