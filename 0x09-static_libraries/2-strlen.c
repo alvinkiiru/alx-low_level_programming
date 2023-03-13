cdca996ab629f010ccfafbd6c24d34391c13d305
Alvin Kiiru <alvinkiiru@gmail.com>
2023-03-13 22:15:32 +0300

    3

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * strlen: returns the length of a string
+ * @s: string
+ * Return: long
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+
+	return (longi);
+}
+
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *islower: checks for lowercase character
+ *c:char to check
+ * Return: 1 if lowercase int,  0 otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
