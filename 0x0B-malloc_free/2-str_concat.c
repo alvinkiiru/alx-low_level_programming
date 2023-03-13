0460e5cb8cef2ed879b9c6c8ca3ce9a8453078d1
Alvin Kiiru <alvinkiiru@gmail.com>
2023-03-13 22:39:26 +0300

    strconcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,13 +3,14 @@
 #include <stdlib.h>
 		
 /**
- * str_concat: get and add end inputs		
- * @s1: input one to concat		
- * @s2: input two to concat
- * Return: always 0 (success)
- *
+		
+ * str_concat: adds input ends together
+ * s1: input one		
+ * s2: input two
+ * Return: concat of s1 and s2
+ *		
  */
-
+		
 char *str_concat(char *s1, char *s2)
 		
 {
@@ -77,3 +78,4 @@ char *str_concat(char *s1, char *s2)
 	return (conct);
 		
 }
+
