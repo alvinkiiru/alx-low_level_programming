b16aae4fc5648f90a6926e86ddde4a6d2c5ae34d
Alvin Kiiru <alvinkiiru@gmail.com>
2023-03-13 22:32:39 +0300

    6

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * _abs: absolute value of an int
+ * n: int to be checked
+ * Return: -n
+ *
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
+
